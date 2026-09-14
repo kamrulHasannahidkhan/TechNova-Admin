d3d11b344d42ed16e2fdc85d7628437a6e43f7bd
kamrulHasannahidkhan <134895166+kamrulHasannahidkhan@users.noreply.github.com>
2026-09-15 03:52:20 +0600

    fix CORS: allow Authorization header for authenticated routes

--- lib/cors.ts
+++ lib/cors.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 export function withCors(res: NextResponse) {
   res.headers.set("Access-Control-Allow-Origin", process.env.CLIENT_APP_URL || "*");
   res.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
   return res;
 }
